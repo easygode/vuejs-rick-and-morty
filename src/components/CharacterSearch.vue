d7237724b9816bffdede53627e65abe68ae7005e
easygode <bimalbenvenuti@gmail.com>
2023-03-29 10:26:47 +0200

    feature: reset search

--- src/components/CharacterSearch.vue
+++ src/components/CharacterSearch.vue
@@ -13,6 +13,12 @@ export default {
             //console.log('modulo inviato');
             //console.log(this.store.searchKey);
             // }
+        reset() {
+            console.log('svuoto i campi');
+            this.store.searchKey = '';
+            this.store.searchStatus = '';
+            this.$emit('search');
+        }
         }
     }
 </script>
@@ -21,24 +27,24 @@ export default {
     <div class="container">
         <form @submit.prevent="$event => $emit('search')">
             <div class="row justify-content-center g-1">
-            <div class="mb-3 col-12 col-sm-auto">
-                <label for="search-character" class="form-label d-none">Search Character</label>
-                <input type="text" class="form-control" id="search-character" placeholder="Search Character" v-model="store.searchKey">
+                <div class="mb-3 col-12 col-sm-auto">
+                    <label for="search-character" class="form-label d-none">Search Character</label>
+                    <input type="text" class="form-control" id="search-character" placeholder="Search Character" v-model="store.searchKey">
+                </div>
+                <div class="mb-3 col-12 col-sm-auto">
+                    <label for="search-status" class="form-label d-none">Select Status</label>
+                    <select class="form-select" id="search-status" v-model="store.searchStatus">
+                        <option selected value>Select Status</option>
+                        <option>alive</option>
+                        <option>dead</option>
+                        <option>unknown</option>
+                    </select>
+                </div>
+                <div class="mb-3 col-12 col-sm-auto">
+                    <button type="submit" class="btn btn-primary me-1">Search</button>
+                    <button type="reset" class="btn btn-secondary" @click="reset">Reset</button>
+                </div>
             </div>
-            <div class="mb-3 col-12 col-sm-auto">
-                <label for="search-status" class="form-label d-none">Select Status</label>
-                <select class="form-select" id="search-status" v-model="store.searchStatus">
-                    <option selected value>Select Status</option>
-                    <option>alive</option>
-                    <option>dead</option>
-                    <option>unknown</option>
-                </select>
-            </div>
-            <div class="mb-3 col-12 col-sm-auto">
-                <button type="submit" class="btn btn-primary me-1">Search</button>
-                <button type="reset" class="btn btn-secondary">Reset</button>
-            </div>
-        </div>
         </form>
     </div>
 </template>
